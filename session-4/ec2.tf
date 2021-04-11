3f85fb13e4429cb732deca92fbf6d786e80320c2
suusardzh <suusardzh@gmail.com>
2021-04-11 23:37:03 +0000

    modified readme and created data-source

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -1,11 +1,16 @@
 resource "aws_instance" "first_ec2" {
-  ami                    = "ami-05d72852800cbf29e"
+  ami                    = ""
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.first_sg.id]
-  key_name               = "suusars-mac-key"
+  key_name               = aws_key_pair.first_key.name
   user_data              = file("user_data.sh")
 
   tags = {
     Name = var.env
   }
+}
+
+resource "aws_key_pair" "first_key" {
+  key_name   = "Terraform-Server"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
