9b7ece5131deeb1212df1566572b7435ed488ee1
suusardzh <suusardzh@gmail.com>
2021-04-16 08:19:30 +0000

    added new resource

--- session-4-task/main.tf
+++ session-4-task/main.tf
@@ -10,6 +10,28 @@ resource "aws_vpc" "first_vpc" {
 }
  
 
+### natgateway eip
+resource "aws_eip" "nat-gw-eip" {
+  vpc      = true
+
+  tags = {
+    Name = "nat eip"
+    Env = var.env
+    Project = "VPC"
+  }
+}
 
 
+### natgateway
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat-gw-eip.id
+  subnet_id     = aws_subnet.public_subnet-01.id
+
+  tags = {
+    Name = "nat_gw"
+    Env = var.env
+    Project = "VPC"
+  }
+}
 
