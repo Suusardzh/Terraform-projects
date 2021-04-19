0abfa6bbf1f91709fc055057d1c331152d354245
suusardzh <suusardzh@gmail.com>
2021-04-19 00:07:26 +0000

    changed sg

--- session-5/dev/sg.tf
+++ session-5/dev/sg.tf
@@ -8,11 +8,11 @@ resource "aws_security_group" "first_sg" {
 }
 
 resource "aws_security_group_rule" "web_ingress" {
-  count = 3
+  count = length(var.web_sg_tcp_ports)   ## the values is 3 because "web_sg_tcp_ports" list has 3 values
   type = "ingress"
   protocol = "tcp"
   security_group_id = aws_security_group.first_sg.id
   from_port = element(var.web_sg_tcp_ports, count.index)
   to_port   = element(var.web_sg_tcp_ports, count.index)
-  cidr_blocks = ["0.0.0.0/0"] 
+  cidr_blocks = [element(var.web_sg_tcp_ports_cidr, count.index)]
 }
