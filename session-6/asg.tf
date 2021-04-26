f397e483f39e2330665976fa025a5a01284d0989
suusardzh <suusardzh@gmail.com>
2021-04-26 00:17:26 +0000

    session-6 modified

---
+++ session-6/asg.tf
@@ -0,0 +1,17 @@
+resource "aws_autoscaling_group" "webserver-sg" {
+  name                      = "${var.env}-webserver-asg"
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity          = 1
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.webserver_lc.name
+  vpc_zone_identifier       = data.aws_subnet_ids.default.ids
+
+}
+
+resource "aws_autoscaling_attachment" "webserver_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.webserver-sg.id
+  alb_target_group_arn   = aws_lb_target_group.webserver_tg.arn
+}
