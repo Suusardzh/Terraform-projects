53bad6bb57b824653b50e69fc0999ac50941ad52
suusardzh <suusardzh@gmail.com>
2021-05-01 23:14:50 +0000

    session-7 created

---
+++ session-7/backend/rds.tf
@@ -0,0 +1,15 @@
+resource "aws_db_instance" "rds" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  indentifier          = "${var.env}-instance"                                                                                                        
+  name                 = "wordpress"
+  username             = "admin"
+  password             = random_password.password.result
+  skip_final_snapshot  = true
+  final_snapshot_identifier = 
+  vpc_security_group_ids = []
+  publicly_accessible    = false
+}           
---
+++ session-7/frontend/launch_conf.tf
@@ -0,0 +1,32 @@
+resource "aws_launch_configuration" "webserver_lc" {
+  name          = "${var.env}-webserver_lc"
+  image_id      = data.aws_ami.amazon_linux2.id
+  instance_type = var.instance_type
+  user_data = data.template_file.webserver.rendered
+  security_groups = [aws_security_group.webserver_sg.id]
+}
+
+resource "aws_security_group" "webserver_sg"{
+    name = "${var.env}-webserver_sg"
+    description = "Allow HTTP traffic"
+}   ##vpc must be here unless default one
+
+resource "aws_security_group_rule" "http_from_lb" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.webserver_sg.id
+
+}
+
+resource "aws_security_group_rule" "webserver_egress" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.webserver_sg.id
+
+}
