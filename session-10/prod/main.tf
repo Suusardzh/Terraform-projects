ea10f3c5806f462c4466130b2b5be328b2ba373d
suusardzh <suusardzh@gmail.com>
2021-05-21 20:56:22 +0000

    modified prod directory

--- session-10/prod/main.tf
+++ session-10/prod/main.tf
@@ -1,7 +1,7 @@
 module "s3_module" {
 
     ### remote module
-    source = "github.com/Suusardzh/Terraform-projects/modules/s3"
+    source = "github.com/Suusardzh/Terraform-projects/modules/ec2"
 
     env = "prod"
     ami = "ami-0518bb0e75d3619ca"
