c73482b7ac24273544f0db591e50e963e00c3b79
suusardzh <suusardzh@gmail.com>
2021-05-21 21:04:49 +0000

    changed ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -2,7 +2,5 @@ resource "aws_instance" "first" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = "suusars-mackey"
-  tags = {
-    Name = "${var.env}-Instance"
-  }
+  tags = local.tags
 }
