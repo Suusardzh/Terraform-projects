2fed29023942d63fab48335e929a2605cb9e8433
suusardzh <suusardzh@gmail.com>
2021-05-21 20:49:17 +0000

    changed stage directory

--- session-10/stage/backend.tf
+++ session-10/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aws-session-terraform-april-suusar"
-    key    = "session-10/dev/main.tfstate"
+    key    = "session-10/stage/main.tfstate"
     region = "us-east-1"
   }
 }
--- session-10/stage/main.tf
+++ session-10/stage/main.tf
@@ -3,6 +3,6 @@ module "s3_module" {
     ### remote module
     source = "github.com/Suusardzh/Terraform-projects/modules/s3"
 
-    env = "dev"
+    env = "stage"
     
 }
