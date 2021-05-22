274984d63369eb7750a0849b8408235bdd5296ae
suusardzh <suusardzh@gmail.com>
2021-05-22 22:09:10 +0000

    fixed main.tf prod

--- session-10/prod/main.tf
+++ session-10/prod/main.tf
@@ -1,11 +1,11 @@
-module "s3_module" {
+module "vpc_module" {
 
     ### remote module
     source = "github.com/Suusardzh/Terraform-projects/modules/vpc"
 
     env = "prod"
-    ami = "ami-0518bb0e75d3619ca"
-    instance_type = "t2.micro"
+    // ami = "ami-0518bb0e75d3619ca"
+    // instance_type = "t2.micro"
     cidr_block = "10.0.0.0/0"
     
 }
