7e7979c153c829ee73715eaa83d22659c40f3c47
suusardzh <suusardzh@gmail.com>
2021-05-22 22:06:24 +0000

    updated

--- session-10/prod/main.tf
+++ session-10/prod/main.tf
@@ -1,10 +1,11 @@
 module "s3_module" {
 
     ### remote module
-    source = "github.com/Suusardzh/Terraform-projects/modules/ec2"
+    source = "github.com/Suusardzh/Terraform-projects/modules/vpc"
 
     env = "prod"
     ami = "ami-0518bb0e75d3619ca"
     instance_type = "t2.micro"
+    cidr_block = "10.0.0.0/0"
     
 }
