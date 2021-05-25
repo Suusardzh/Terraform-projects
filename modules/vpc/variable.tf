efd66ecffc2e96a986c4bca803eca60c7f859a24
suusardzh <suusardzh@gmail.com>
2021-05-25 23:02:14 +0000

    added rt table to vpc module

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -3,4 +3,5 @@
 variable "env" {}
 variable "cidr_block" {}
 variable "public_subnet_cidr_block_01" {}
+variable "rt_cidr_block" {}
 
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -22,3 +22,22 @@ resource "aws_subnet" "public_subnet-01" {
 
   tags = local.tags
 }
+
+
+### public_route_table
+resource "aws_route_table" "pub_rt" {
+  vpc_id = aws_vpc.first_vpc.id
+  route {
+    cidr_block = var.rt_cidr_block
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = local.tags
+}
+
+###aws_public_route_table_subnet_association
+
+resource "aws_route_table_association" "public_subnet-01" {
+  subnet_id      = aws_subnet.public_subnet-01.id
+  route_table_id = aws_route_table.pub_rt.id
+}
